c9c1cd2ca6c615789465edf471253b3d9d3c34f0
HugoOtth <hugo.otth@gmail.com>
2025-03-07 19:45:00 -0500

    Refactor payload website template to domain

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'Lighting Designer and Operator',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/website-hero.jpg`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Hugo Otth Portfolio',
+  title: 'Hugo Otth Portfolio',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
