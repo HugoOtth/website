a393a2cb6d503079249c96b4b829b5fde9e4d6e0
HugoOtth <hugo.otth@gmail.com>
2025-03-08 13:40:56 -0500

    Migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250308_183843 from './20250308_183843';
+
+export const migrations = [
+  {
+    up: migration_20250308_183843.up,
+    down: migration_20250308_183843.down,
+    name: '20250308_183843'
+  },
+];
