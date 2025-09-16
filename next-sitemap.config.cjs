b224168e82c5102d5fe7b6f6225399ef6992439b
HugoOtth <hugo.otth@gmail.com>
2025-09-15 20:53:23 -0400

    maybe fix sitemaps.

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,13 +1,13 @@
 const SITE_URL =
   process.env.NEXT_PUBLIC_SERVER_URL ||
   process.env.VERCEL_PROJECT_PRODUCTION_URL ||
-  'https://example.com'
+  'https://www.hugo-otth.com'
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: SITE_URL,
   generateRobotsTxt: true,
-  exclude: ['/posts-sitemap.xml', '/pages-sitemap.xml', '/*', '/posts/*'],
+  exclude: [],
   robotsTxtOptions: {
     policies: [
       {
