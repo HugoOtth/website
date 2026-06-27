633d9d0a1db40ebfc603cb521b3c4160a92e7ab5
HugoOtth <hugo.otth@gmail.com>
2026-06-26 22:50:07 -0400

    deploy fix?

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,14 @@
+import * as migration_20250307_mark_applied from './20250307_mark_applied'
 import * as migration_20250308_183843 from './20250308_183843'
 import * as migration_20250308_210559 from './20250308_210559'
 import * as migration_20260627_022630 from './20260627_022630'
-import * as migration_20260627_mark_applied from './20260627_mark_applied'
 
 export const migrations = [
+  {
+    up: migration_20250307_mark_applied.up,
+    down: migration_20250307_mark_applied.down,
+    name: '20250307_mark_applied',
+  },
   {
     up: migration_20250308_183843.up,
     down: migration_20250308_183843.down,
@@ -18,10 +23,5 @@ export const migrations = [
     up: migration_20260627_022630.up,
     down: migration_20260627_022630.down,
     name: '20260627_022630',
-  },
-  {
-    up: migration_20260627_mark_applied.up,
-    down: migration_20260627_mark_applied.down,
-    name: '20260627_mark_applied',
   },
 ]
